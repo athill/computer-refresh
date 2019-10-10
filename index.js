b6bae7ab21e186b02d21a33d9551044070fd5fff
Andy Hill <athill@iu.edu>
2019-10-09 20:49:35 -0400

    modularized some things, fixed some things, added 'listings', some things are still broken

--- index.js
+++ index.js
@@ -1,10 +1,84 @@
 const { execSync } = require('child_process');
-const { existsSync, readFileSync } = require('fs-extra');
+const { existsSync, readFileSync, readdirSync, writeFileSync } = require('fs-extra');
+const { join } = require('path');
 const yaml = require('js-yaml');
+const copy = require('recursive-copy');
 
-const { copyFilesWithStructure, mkdir } = require('./utils');
+const { copyFilesWithStructure, fixPath, mkdir } = require('./utils');
 
 let configfile = 'config.yml';
+
+const handleBackupMapping = async (mapping, destination) => {
+  const from = fixPath(mapping.from);
+  const to = join(destination, mapping.to);
+  let copyAll = true;
+  if ('files' in mapping) {
+    copyAll = false;
+    console.log(`copy files to ${to}`);
+    try {
+      await copyFilesWithStructure(from, to, mapping.files);  
+    } catch (error) {
+      console.log('error: failed to copy files', {from, to, files: mapping.files}, error);
+      process.exit(1);
+    }
+    
+  }
+  if ('filter' in mapping) {
+    copyAll = false;
+    console.log(`copy filter to ${to}`);
+    try {
+      console.log({ from, to });
+      await copy(from, to, { dot: true, filter: mapping.filter });  
+    } catch (error) {
+      console.log('error: filed to copy filtered files', {from, to}, error);
+      process.exit(1);
+    }
+    
+  }
+  // if no pattern or file list, copy all contents
+  if (copyAll) {
+    console.log(`copy all to ${to}`);
+    try {
+      await copy(from, to, { dot: true });  
+    } catch (error) {
+      console.log('error: failed to copy all files', {from, to}, error);
+      process.exit(1);
+    }
+    
+  }
+};
+
+const backupListings = async (listings, destination) => {
+  console.log({listings, destination});
+  Object.keys(listings).forEach(label => {
+    const listing = readdirSync(fixPath(listings[label]));
+    writeFileSync(join(destination, `${label}.txt`), listing.join('\n'));
+    console.log(listing);
+  })
+};
+
+
+const backupToDestination = async config => {
+  const destination = fixPath(config.destination);
+  if (config.listings) {
+    backupListings(config.listings, destination);
+  }
+  // process.exit(1);
+  // if (config.mappings) {
+  //   config.mappings.forEach(async mapping => {
+  //     handleBackupMapping(mapping, destination);
+  //   });
+  // }  
+};
+
+
+const main = async (config) => {
+  for (let label in config) {
+    console.log('***' + label + '***');    
+    await backupToDestination(config[label]);
+  }
+};
+
 
 if (process.argv.length === 3) {
   configfile = process.argv[2]
@@ -17,6 +91,7 @@ try {
   console.log('Cannot load config file', e);
   process.exit(1);
 }
+main(config);
 
 // console.log(buildFind(config.secure.mappings['~/Code^app-config']));
 
--- utils.js
+++ utils.js
@@ -2,22 +2,36 @@ const { copy, existsSync, mkdirSync } = require('fs-extra');
 const { dirname, join } = require('path');
 const mkdirp = require('mkdirp');
 
+const homedir = require('os').homedir();
 
 const mkdir = path => existsSync(path) || mkdirp.sync(path);
 
-const copyFilesWithStructure = (paths, backupdir) => {
-  paths.forEach(path => {
-    const backuppath = join(backupdir, path);
+const fixPath = path => path.replace('~', homedir);
+
+const copyFilesWithStructure = async (from, to, paths) => {
+  // console.log('cfws', { paths, to });
+  // return;
+  await process.chdir(from);
+  paths.forEach(async path => {
+    const backuppath = join(to, path);
     const parent = dirname(backuppath);
-    console.log(parent);
-    mkdir(parent);
+    try {
+      mkdir(parent);  
+    } catch (error) {
+      console.log(`error: could not mkdir ${parent}`, error);
+      process.exit(1);
+    }
+    
     if (path) {
-      copy(path, backuppath);
+      console.log('copying file in structure', { path, backuppath });
+      await copy(path, backuppath);
+      console.log(`Failed to copy ${path} to ${backuppath}`, error);
     }
   });   
 };
 
 module.exports = {
+  fixPath,
   mkdir,
   copyFilesWithStructure
 };
