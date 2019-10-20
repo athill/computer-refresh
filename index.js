9c600b8e77f5be83ffa5014f14c2343c69165c64
Andy Hill <athill@iu.edu>
2019-10-20 17:18:44 -0400

    things seem to be working

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const { execSync } = require('child_process');
 const { existsSync, readFileSync, readdirSync, writeFileSync } = require('fs-extra');
 const { join } = require('path');
 const yaml = require('js-yaml');
-// const copy = require('recursive-copy');
 
-const { copyFilesWithStructure, fixPath, mkdir } = require('./utils');
+const { copyFilesWithStructure, fixPath, mkdir, quotePath } = require('./utils');
+
+const log4js = require('log4js');
+const logger = log4js.getLogger();
+logger.level = process.env.LOG_LEVEL || 'error';
+
 
 let configfile = 'config.yml';
+
 
 const handleBackupMapping = async (mapping, destination) => {
   const from = fixPath(mapping.from);
@@ -14,57 +19,90 @@ const handleBackupMapping = async (mapping, destination) => {
   let copyAll = true;
   if ('files' in mapping) {
     copyAll = false;
-    console.log(`copy files to ${to}`);
+    logger.info(`copy files`, { from , to });
     try {
       await copyFilesWithStructure(from, to, mapping.files);  
     } catch (error) {
-      console.log('error: failed to copy files', {from, to, files: mapping.files}, error);
+      logger.error('failed to copy files', {from, to, files: mapping.files}, error);
       process.exit(1);
     }
     
   }
   if ('filter' in mapping) {
     copyAll = false;
-    console.log(`copy filter to ${to}`);
     try {
-      console.log({ from, to });
-      execSync(`cp -rf ${from}/${mapping.filter} ${to}`);
-      // await copy(from, to, { dot: true, filter: mapping.filter });  
+      execSync(`cp -rf ${quotePath(join(from, mapping.filter))} "${to}"`);
     } catch (error) {
-      console.log('error: filed to copy filtered files', {from, to}, error);
+      logger.error('Failed to copy filtered files', {from, to}, error);
       process.exit(1);
     }
     
   }
   // if no pattern or file list, copy all contents
   if (copyAll) {
-    console.log(`copy all to ${to}`);
     try {
-      execSync(`cp -rf ${from} ${to}`); 
+      execSync(`cp -rf ${quotePath(join(from, '*'))} "${to}"`); 
     } catch (error) {
-      console.log('error: failed to copy all files', {from, to}, error);
+      logger.error('Failed to copy all files', {from, to}, error);
       process.exit(1);
     }
-    
+     
   }
 };
 
+const buildFindCommand = config => {
+  let findCommand = 'find .';
+  let excludeDirectories = [];
+  let includeDirectories = [];
+  if (config.directories) {
+    if (config.directories.exclude) {
+      excludeDirectories = config.directories.exclude.map(exclude => ` -type d -name ${exclude} -prune -not -name  ${exclude}`);
+      findCommand += excludeDirectories.join(' -o');
+    }
+    if (config.directories.include) {
+      if (excludeDirectories.length) {
+        findCommand += ' -o';
+      }
+      includeDirectories = config.directories.include.map(include => ` -type d -name ${include}`);
+      findCommand += includeDirectories.join(' -o');
+    }
+  }
+  if (config.files.include) {
+    if (excludeDirectories.length || includeDirectories.length) {
+      findCommand += ' -o';
+    }
+      const includeFiles = config.files.include.map(include => ` -type f -name ${include}`);
+      findCommand += includeFiles.join(' -o');    
+  }
+  return findCommand;
+};
+
+const backupAppConfig = async (config, destination) => {
+  const from = fixPath(config.from);
+  await process.chdir(from);
+  const to = join(destination, config.to);
+  const findCommand = buildFindCommand(config);
+  const paths = execSync(findCommand, { encoding: 'utf-8' }).split('\n');
+  await copyFilesWithStructure(from, to, paths);
+};
+
 const backupListings = async (listings, destination) => {
-  console.log({listings, destination});
+  logger.debug('back up listing', {listings, destination});
   Object.keys(listings).forEach(label => {
     const listing = readdirSync(fixPath(listings[label]));
-    writeFileSync(join(destination, `${label}.txt`), listing.join('\n'));
-    console.log(listing);
+    writeFileSync(join(destination, `${label}.txt`), listing.join('\n')); 
   })
 };
 
 
 const backupToDestination = async config => {
   const destination = fixPath(config.destination);
+  if ('app-config' in config) {
+    backupAppConfig(config['app-config'], destination);
+  }
   if (config.listings) {
     backupListings(config.listings, destination);
   }
-  // process.exit(1);
   if (config.mappings) {
     config.mappings.forEach(async mapping => {
       handleBackupMapping(mapping, destination);
@@ -72,14 +110,12 @@ const backupToDestination = async config => {
   }  
 };
 
-
 const main = async (config) => {
   for (let label in config) {
-    console.log('***' + label + '***');    
+    logger.info('***' + label + '***');    
     await backupToDestination(config[label]);
   }
 };
-
 
 if (process.argv.length === 3) {
   configfile = process.argv[2]
@@ -87,52 +123,17 @@ if (process.argv.length === 3) {
 
 try {
   var config = yaml.safeLoad(readFileSync(configfile, 'utf8'));
-  console.log(config);
 } catch (e) {
-  console.log('Cannot load config file', e);
+  logger.error('Cannot load config file', e);
   process.exit(1);
 }
 main(config);
 
-// console.log(buildFind(config.secure.mappings['~/Code^app-config']));
 
+/**
+ * start of app-config
+ */
 
-// const usage='intellij-backup <code-root> <backup-dir>';
-
-
-
-
-
-
-
-// const [ , , coderoot, backupdir ] = process.argv;
-
-// if (!existsSync(coderoot)) {
-//   console.log(`Error: Invalid path: ${coderoot}`);
-//   process.exit(1);
-// }
-
-// if (!existsSync(backupdir)) {
-//   mkdir(backupdir);
-// }
-
-// process.chdir(coderoot);
-
+// ** get git remote origin?
 // const paths = execSync('find . -type d -name node_modules -prune -not -name  node_modules -o -type d -name target -prune -not -name target  -o -name .idea -o -name "*.env"', { encoding: 'utf-8' }).split('\n');
-
-
-// copyFilesWithStructure(paths, backupdir);
-// paths.forEach(path => {
-//   const backuppath = join(backupdir, path);
-//   const parent = dirname(backuppath);
-//   console.log(parent);
-//   mkdir(parent);
-//   if (path) {
-//     copy(path, backuppath);
-//   }
-// }); 
-
-
-
-
-
+// 
--- utils.js
+++ utils.js
@@ -1,46 +1,63 @@
 const { execSync } = require('child_process');
-const { copy, existsSync, lstatSync, mkdirSync } = require('fs-extra');
-const { dirname, join } = require('path');
+const { existsSync, lstatSync, mkdirSync } = require('fs-extra');
+const { basename, dirname, join } = require('path');
 const mkdirp = require('mkdirp');
+const isGlob = require('is-glob');
+
+const log4js = require('log4js');
+const logger = log4js.getLogger();
+logger.level = process.env.LOG_LEVEL || 'error';
 
 const homedir = require('os').homedir();
 
 const mkdir = path => existsSync(path) || mkdirp.sync(path);
 
+const quotePath = path => isGlob(basename(path)) ? join(`"${dirname(path)}"`, basename(path)) : `"${path}"`;
+
+// const copy = (flags, from to) => {
+//   execSync(`cp -f ${from} "${toDir}"`);
+// }
+
 const fixPath = path => path.replace('~', homedir);
 
 const copyFilesWithStructure = async (from, to, paths) => {
-  // console.log('cfws', { paths, to });
-  // return;
   await process.chdir(from);
   paths.forEach(async path => {
     const backuppath = join(to, path);
     const parent = dirname(backuppath);
+    logger.info(parent);
     try {
       mkdir(parent);  
     } catch (error) {
-      console.log(`error: could not mkdir ${parent}`, error);
+      logger.error(`could not mkdir ${parent}`, error);
       process.exit(1);
     }
     
     if (path) {
-      const isLink = lstatSync(path).isSymbolicLink();
-      console.log('copying file in structure', { path, backuppath, from, isLink });
+      const stats = lstatSync(path);
+      const isLink = stats.isSymbolicLink();
+      logger.debug('copying file in structure', { path, backuppath, from, isLink });
       try {
         if (!isLink || !existsSync(backuppath)) {
           if (isLink) {
-            execSync(`cp -Lf ${path} ${backuppath}`);              
+            logger.debug('link', path);
+            execSync(`cp -Lf "${path}" "${backuppath}"`);     
+          } else if (stats.isDirectory()) {
+            logger.debug('directory', path);
+            execSync(`cp -rf "${path}" "${backuppath}"`);
           } else {
-            execSync(`cp -rf ${path} ${backuppath}`);  
-          }
+            logger.debug('file', { path, backuppath, dirname: dirname(path) });
+            // if (dirname(path) !== '.') {
+            //   mkdir(dirname(path));
+            // }
 
+            execSync(`cp -f ${quotePath(path)} "${backuppath}"`);
+          }
         }
       } catch (error) {
-        console.log(`Failed to copy ${path} to ${backuppath}`, error);
+        logger.error(`Failed to copy ${path} to ${backuppath}`, error);
         process.exit(1);
       }
-      
-
     }
   });   
 };
@@ -48,5 +65,6 @@ const copyFilesWithStructure = async (from, to, paths) => {
 module.exports = {
   fixPath,
   mkdir,
+  quotePath,
   copyFilesWithStructure
 };
