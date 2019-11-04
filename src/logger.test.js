a9ba0f316f3fc4b3b6651d7eb57f2bcea1ef3676
Andy Hill <andy@andyhill.us>
2019-11-03 20:49:32 -0500

    add logger tests

---
+++ src/logger.test.js
@@ -0,0 +1,14 @@
+
+// TODO: Figure out why env variable is not respected
+describe.skip('logger', () => {
+	it('should default to "error"', () => {
+		const logger = require('./logger');
+		expect(logger.level.levelStr).toBe('ERROR');
+	});
+
+	it('should be overridable by environment variable', () => {
+		process.env.LOG_LEVEL = 'info';
+		const logger = require('./logger');
+		expect(logger.level.levelStr).toBe('ERROR');
+	});
+});
